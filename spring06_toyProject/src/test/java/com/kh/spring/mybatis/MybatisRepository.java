98e8a7d8aba979af8a4ec47d922bbb71ab1f6328
drakart <oooklyt@gmail.com>
2021-10-19 15:23:17 +0900

    21/10/19 mybatis interface 실습, spring MVC 패턴 시작(경로 설정 및 회원가입 실습)

---
+++ spring06_toyProject/src/test/java/com/kh/spring/mybatis/MybatisRepository.java
@@ -0,0 +1,31 @@
+package com.kh.spring.mybatis;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import com.kh.spring.member.model.dto.Member;
+
+@Mapper
+public interface MybatisRepository {
+	
+	@Select("select password from member where user_id = #{userId}")
+	String selectPasswordByUserId(String userId);
+	
+	@Select("select * from member where user_id = #{userId}")
+	Member selectMemberByUserId(String userId);
+	
+	@Select("select * from member inner join rent_master using(user_id) where user_id=#{user_id}")
+	List<Map<String,Object>> selectRentAndMemberByUserId(String userId);
+	
+	List<Map<String,Object>> selectRentBookByUserId(String userId);
+
+	Member insertWithDto(Member member);
+	
+	@Delete("delete from rent_master where user_id = #{userId}")
+	String delete(String userId);
+	
+}
