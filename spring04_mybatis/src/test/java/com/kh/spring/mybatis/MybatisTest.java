714a146a5bc0285efa94aad8b65675cc09035a08
drakart <oooklyt@gmail.com>
2021-10-19 10:14:14 +0900

    21/10/19 mybatis typeHandler 실습

--- spring04_mybatis/src/test/java/com/kh/spring/mybatis/MybatisTest.java
+++ spring04_mybatis/src/test/java/com/kh/spring/mybatis/MybatisTest.java
@@ -234,6 +234,22 @@ public class MybatisTest {
 		session.update(NAMESPACE + "dynamicSet", member);
 	}
 	
+	@Test
+	public void procedureUseTypeHandler() {
+		session.insert(NAMESPACE + "procedureUseTypeHandler", 
+				Map.of("userId","DEV","title","타입핸들러와 마이바티스","rentBookCnt",2
+						,"bkIdxs",List.of("100001","100002")));
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 
 }
